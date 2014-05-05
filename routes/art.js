da697fa5014a81a29450a9e3d4581db5a0e0104a
Sam Gussman <sgussman@stanford.edu>
2014-05-04 20:31:55 -0700

    Route to the Art page and correctly display the info from the database. Also fixed the link to the artist page being anywhere you click. Now it's just when you click on the artist's name.

--- routes/art.js
+++ routes/art.js
@@ -1,4 +1,15 @@
+var models = require('../models.js');
+
 exports.view = function(req, res){
+	var title = req.params.title;
 	console.log('Opening Art Page.');
-	res.render('art');
+	models.Art.find({"title" : title})
+	.exec(afterQueryRender)
+
+	function afterQueryRender(err, results) {
+		if(err) console.log(err);
+		var art = results[0];
+		console.log(art);
+		res.render("art", art);
+	}
 }
