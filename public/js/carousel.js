48a5705863fe47fb050e3fa7a5add08581c7263b
Sam Gussman <sgussman@stanford.edu>
2014-05-08 22:09:43 -0700

    Working on the carousel. Incorporated a jcarousel, and fixed the navbar to the bottom of the screen.

---
+++ public/js/carousel.js
@@ -0,0 +1,7 @@
+'use strict';
+
+$(function() {
+    $('.jcarousel').jcarousel({
+        list: '.jcarousel-list'
+    });
+});
--- routes/gallery.js
+++ routes/gallery.js
@@ -1,4 +1,15 @@
+var models = require('../models.js');
+
 exports.view = function(req, res){
-	console.log('Opening the Gallery page.');
-	res.render('gallery');
+	var title = req.params.title;
+	console.log('Opening Gallery Page.');
+	models.Art.find({"title" : title})
+	.exec(afterQueryRender)
+
+	function afterQueryRender(err, results) {
+		if(err) console.log(err);
+		var art = results[0];
+		console.log(art);
+		res.render("gallery", art);
+	}
 }
