bedaab966b3c37add207cfd14143c5329e742924
Sam Gussman <sgussman@stanford.edu>
2014-05-12 12:19:02 -0700

    working on ./routes/class error.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var mongoose = require('mongoose');
 var launch = require('./routes/launch');
 var art = require('./routes/art');
 var artist = require('./routes/artist');
-var class_page = require('./routes/class');
+var class_page = require('./routes/class_page');
 var gallery = require('./routes/gallery');
 var twitter = require('./routes/twitter');
 
