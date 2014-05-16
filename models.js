d5de23fea8335a8e1aeb1fd5f0b6f88b55a35085
Sam Gussman <sgussman@stanford.edu>
2014-05-15 23:24:14 -0700

    trying to make the website correctly render 2 or more artist. Also filling in the data for the database.

--- models.js
+++ models.js
@@ -3,13 +3,14 @@ var Mongoose = require('mongoose');
 
 var ArtSchema = new Mongoose.Schema({
 	"title": String,
-	"artist": String,
+	"artist": [String],
 	"bio": [String],
 	"about_art": [String],
 	"photos": [String],
-	"artist_pic": String,
+	"artist_pic": [String],
 	"art_pic": String,
-	"twitter_handle": String
+	"twitter_handle": String,
+	"website": String
 });
 
 exports.Art = Mongoose.model('Art', ArtSchema);
